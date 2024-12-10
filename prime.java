ba47c80841fc7ae3ee47cc62094c618d477a02f2
arav39marri <aravindmarripelli@gmail.com>
2024-12-11 00:00:03 +0530

    cmd

--- prime.java
+++ prime.java
@@ -1,10 +1,1 @@
-public class prime {
-
-    public static void main(String[] args) {
-        for(int i = 2;i<=100;i++){
-            int c = 0;
-            
-            
-        }
-    }
-}
+//jkdfks
